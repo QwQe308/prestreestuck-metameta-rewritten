81bd8baf889b22e6b4326f2bbf32e1e2e33dace0
QwQe308 <78683793+QwQe308@users.noreply.github.com>
2022-02-27 08:15:46 +0800

    v0.12

--- js/mod.js
+++ js/mod.js
@@ -7,17 +7,19 @@ let modInfo = {
 	discordLink: "",
 	initialStartPoints: new ExpantaNum (0), // Used for hard resets and new players
 	
-	offlineLimit: 0.1,  // In hours
+	offlineLimit: 0.2,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.11",
+	num: "0.12",
 	name: "",
 }
 
 let changelog = `<h1>更新记录(时不时懒得写):</h1><br>
-	<h3>v0.11</h3><br>
+  <h3>v0.12</h3><br>
+		- 大型重平衡并更新超限.如果你上次更新到了3超限,你会被一定程度回档.当前endgame:1.80e308t.<br><br>
+  <h3>v0.11</h3><br>
 		- 更新超限.当前endgame:3超限.<br><br>
 	<h3>v0.1</h3><br>
 		- 制作超限节点.当前endgame:2超限.之前的更新记录全懒得写了.`
@@ -53,12 +55,12 @@ var displayThings = [
   function(){return `t = ${format(player.m.time)}`},
   function(){return `当前元性质上限: ${format(layers.m.maxValue())}`},
   function(){return `当前点数上限: F1.79e308`},
-    function(){return `当前endgame: 3超限`},
+    function(){return `当前endgame: 1.80e308t,永恒咕咕咕中(`},
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.overflow.total.gte(2)
+	return player.m.time.gte(1.79e308)
 }
 
 
@@ -67,10 +69,18 @@ function isEndgame() {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(360) // Default is 1 hour which is just arbitrarily large
+	return(720) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
 function fixOldSave(oldVersion){
+  if(oldVersion<=0.11){
+    if(player.overflow.total.gte(3)){
+      player.overflow.points = n(3)
+      player.overflow.total = n(3)
+      player.overflow.best = n(3)
+      layerDataReset('m');layerDataReset('dim');layerDataReset('mm')
+    }
+  }
 }
